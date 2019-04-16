1980c09569ba10f1d4363eea1ffad0e8e7bdf04a
Greg Dean <gmdean@buffalo.edu>
2019-04-16 13:54:18 -0400

    moved function for automatically making help to tours for portlets to new portlet componenet

--- html/gui/lib/Portlet.js
+++ html/gui/lib/Portlet.js
@@ -10,45 +10,8 @@ Ext.ux.Portlet = Ext.extend(Ext.Panel, {
     collapsible: true,
     draggable: true,
     cls: 'x-portlet',
-    helpTour: null,
-    helpTourDetails: [],
     initComponent: function() {
-        this.makeHelpTour();
-        this.setHelpTourStart();
-
         Ext.ux.Portlet.superclass.initComponent.apply(this, arguments);
-    },
-    makeHelpTour: function() {
-        var self = this;
-
-        if (self.helpTourDetails.tips !== undefined && self.helpTourDetails.tips.length > 0) {
-            self.helpTour = new Ext.ux.HelpTipTour({
-                title: self.helpTourDetails.title,
-                items: []
-            });
-            self.helpTourDetails.tips.forEach(function(value, key) {
-                var tip = new Ext.ux.HelpTip({
-                    html: value.html,
-                    target: value.target,
-                    position: value.position
-                });
-
-                self.helpTour.items.push(tip);
-            });
-        }
-    },
-    setHelpTourStart: function() {
-        var self = this;
-
-        if (self.helpTourDetails.startAt !== undefined) {
-            new Ext.util.DelayedTask(function(){
-              var target = Ext.select(self.helpTourDetails.startAt);
-              target.on('click', self.helpTipStartCallback, self);
-            }).delay(10);
-        }
-    },
-    helpTipStartCallback: function() {
-        this.helpTour.startTour();
     }
 });
 
