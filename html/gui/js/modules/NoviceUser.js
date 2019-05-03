e4247eeea9561fec41787baf10f27f50231291a5
Greg Dean <gmdean@buffalo.edu>
2019-05-03 08:42:35 -0400

    adding new text for new user tour tips

--- html/gui/js/modules/NoviceUser.js
+++ html/gui/js/modules/NoviceUser.js
@@ -209,14 +209,25 @@ Ext.extend(XDMoD.Module.Summary, XDMoD.PortalModule, {
             title: 'New User Tour',
             items: [
               new Ext.ux.HelpTip({
-                  html: "Welcome to XDMoD",
+                  html: `Welcome to Open XDMoD! Open XDMoD is an open source tool to facilitate the management of
+                         high performance computing resources. It is widely deployed at academic, industrial and governmental
+                         HPC centers. Open XDMoD’s management capabilities include monitoring standard metrics such as
+                         utilization, providing quality of service metrics designed to proactively identify underperforming
+                         system hardware and software,`,
                   target: "#tg_summary",
                   position: "b-t"
               }),
               new Ext.ux.HelpTip({
-                  html: "XDMoD has a tab based navigation.",
+                  html: `The XDMoD User Interface contains a wealth of information and has been organized into tabs
+                         to compartmentalize the data. `,
                   target: "#main_tab_panel .x-tab-panel-header",
                   position: "tl-bl"
+              }),
+              new Ext.ux.HelpTip({
+                  html: `Your are currently on the Summary tab which provides a snapshot overview of selected
+                         data with several small summary charts that can be expanded to full size charts.`,
+                  target: "#tg_summary",
+                  position: "t-b"
               }),
               new Ext.ux.HelpTip({
                   html: "This is a portlet.",
@@ -224,20 +235,27 @@ Ext.extend(XDMoD.Module.Summary, XDMoD.PortalModule, {
                   position: "tl-br"
               }),
               new Ext.ux.HelpTip({
-                  html: "This is the help button.",
+                  html: `Clicking on the Help menu will provide you with the following options: User Manual, FAQ and
+                         YouTube Channel. Clicking on User Manual will direct the user to the XDMoD User Manual.
+                         If help is available for the section of XDMoD you currently are visiting, the User Manual will
+                         automatically navigate to the respective section when it loads.  Clicking on FAQ will take you to
+                         a page containing Frequently Asked  Questions.`,
                   target: "#global-toolbar-dashboard",
                   position: "tl-bl",
-                  listener: {
-                      render: function(){
-                          console.log('before render');
+                  listeners: {
+                      show: function(){
                           Ext.get('help_button').dom.click()
                       }
                   }
               }),
               new Ext.ux.HelpTip({
-                  html: "My Profile button.",
+                  html: `The 'My Profile' button allows you to view and update general settings pertaining to your account.
+                         Your current role will be displayed in the title bar of the My Profile window.
+                         The first section in the My Profile window contains User Information such as: First Name,
+                         Last Name, and Email Address.  All of the aforementioned fields must be specified in order
+                         to update your profile.`,
                   target: "#global-toolbar-profile",
-                  position: "tl-br"
+                  position: "tl-bl"
               })
             ]
         });
