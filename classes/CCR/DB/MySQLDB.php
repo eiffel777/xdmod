4caaa7d6a324b36484a1572da8979c37487a672b
Greg Dean <gmdean@buffalo.edu>
2026-08-31 07:59:17 -0400

    making all mysql db connections use strict mode

--- classes/CCR/DB/MySQLDB.php
+++ classes/CCR/DB/MySQLDB.php
@@ -18,6 +18,17 @@ use Exception;
 class MySQLDB extends PDODB implements iDatabase
 {
     // ------------------------------------------------------------------------------------------
+    // TEMPORARY - remove before merging.
+    //
+    // Forces strict mode on each session so compliance can be tested without changing the
+    // shared dev server's global sql_mode. This is the measured MariaDB 10.6-12.3 default
+    // plus ONLY_FULL_GROUP_BY.
+    // ------------------------------------------------------------------------------------------
+
+    const TEMP_SESSION_SQL_MODE = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,'
+        . 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION,ONLY_FULL_GROUP_BY';
+
+    // ------------------------------------------------------------------------------------------
     // @see iDatabase::__construct()
     // ------------------------------------------------------------------------------------------
 
@@ -28,5 +39,23 @@ class MySQLDB extends PDODB implements iDatabase
             throw new Exception($msg);
         }
         parent::__construct("mysql", $db_host, $db_port, $db_name, $db_username, $db_password, 'charset=utf8');
+    }
+
+    // ------------------------------------------------------------------------------------------
+    // @see iDatabase::connect()
+    //
+    // TEMPORARY - remove along with TEMP_SESSION_SQL_MODE.
+    // ------------------------------------------------------------------------------------------
+
+    public function connect()
+    {
+        $isNewConnection = ( null === $this->_dbh );
+        $dbh = parent::connect();
+
+        if ( $isNewConnection ) {
+            $dbh->exec('SET SESSION sql_mode = ' . $dbh->quote(self::TEMP_SESSION_SQL_MODE));
+        }
+
+        return $dbh;
     }
 }  // class MySQLDB
